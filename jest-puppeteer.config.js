4cfe3c1010a61c0b9d30fccaa4ad4b357781d1a4
shufo <meikyowise@gmail.com>
2020-03-08 20:58:32 +0900

    test(server): run server with log output

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   server: {
-    command: 'mix run --no-halt',
+    command: 'mix run --no-halt -e "require Logger; :timer.sleep(3000); Logger.info(\'test_log\')"',
     port: 5900
   },
 };
