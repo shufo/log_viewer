8a6fd63fffc566c7f31d6b1c0d79e63a2443c6c5
shufo <meikyowise@gmail.com>
2020-03-08 21:00:03 +0900

    test(browser): add test for end to end logging output

--- test/browser/boot.test.js
+++ test/browser/boot.test.js
@@ -1,14 +1,22 @@
 beforeAll(async () => {
-  await page.goto('http://localhost:5900/');
+  await page.goto("http://localhost:5900/", { waitUntil: "domcontentloaded" });
 });
 
 afterAll(async done => {
   done();
 });
 
-it('should display loading page', async () => {
+it("should display loading page", async () => {
   const text = await page.evaluate(
-    () => document.querySelector('#waiting_text').textContent,
+    () => document.querySelector("#waiting_text").textContent
   );
-  expect(text).toBe('Waiting for logs matching the conditions to coming in...');
+  expect(text).toBe("Waiting for logs matching the conditions to coming in...");
+});
+
+it("should get info logs", async () => {
+  await page.waitForSelector("#app_message_key", { visible: true });
+  const log = await page.evaluate(
+    () => document.querySelector("#app_message_key").textContent
+  );
+  expect(log).toBe("test_log");
 });
