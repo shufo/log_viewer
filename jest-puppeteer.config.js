5ea8bc8e3f7521cd0efc12bad55dc376a6ab8c54
shufo <meikyowise@gmail.com>
2020-03-08 21:13:03 +0900

    chore(test): wait 5 seconds for logs output
    For test stability

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   server: {
-    command: 'mix run --no-halt -e "require Logger; :timer.sleep(3000); Logger.info(\'test_log\')"',
+    command: 'mix run --no-halt -e "require Logger; :timer.sleep(5000); Logger.info(\'test_log\')"',
     port: 5900
   },
 };
